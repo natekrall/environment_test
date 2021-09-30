98e3c079a0a08cb4953873a0c0b375eefa08825f
natekrall <natekrall@users.noreply.github.com>
2021-09-30 01:39:26 -0500

    Added Integration and Unit Tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -2,11 +2,47 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
+  scenario 'valid title' do
     visit new_book_path
-    fill_in 'Title', with: 'harry potter'
+    fill_in 'Title', with: 'Manufacturing Consent'
+    fill_in 'Author', with: 'Noam Chomsky'
+    fill_in 'Price', with: '18.49'
+    fill_in 'PublicationDate', with: '1988-17-03'
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('harry potter')
+    expect(page).to have_content('Manufacturing Consent')
+  end
+
+  scenario 'valid author' do
+    visit new_book_path
+    fill_in 'Title', with: 'Manufacturing Consent'
+    fill_in 'Author', with: 'Noam Chomsky'
+    fill_in 'Price', with: '18.49'
+    fill_in 'PublicationDate', with: '1988-17-03'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('Noam Chomsky')
+  end
+
+  scenario 'valid price' do
+    visit new_book_path
+    fill_in 'Title', with: 'Manufacturing Consent'
+    fill_in 'Author', with: 'Noam Chomsky'
+    fill_in 'Price', with: '18.49'
+    fill_in 'PublicationDate', with: '1988-17-03'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('18.49')
+  end
+
+  scenario 'valid publication date' do
+    visit new_book_path
+    fill_in 'Title', with: 'Manufacturing Consent'
+    fill_in 'Author', with: 'Noam Chomsky'
+    fill_in 'Price', with: '18.49'
+    fill_in 'PublicationDate', with: '1988-17-03'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('1988-17-03')
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -2,15 +2,49 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'Manufacturing Consent',
+                        author: 'Noam Chomsky',
+                        price: "18.49",
+                        publicationDate: '1988-03-17')
   end
-
-  it 'is valid with valid attributes' do
+  #Title
+  it 'is valid with valid title' do
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
+  it 'is not valid without a title' do
     subject.title = nil
     expect(subject).not_to be_valid
   end
+
+  #Author
+  it 'is valid with valid author' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  #Price
+  it 'is valid with valid price' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  #PublicationDate
+  it 'is valid with valid publication date' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a publication date' do
+    subject.publicationDate = nil
+    expect(subject).not_to be_valid
+  end
+
 end
